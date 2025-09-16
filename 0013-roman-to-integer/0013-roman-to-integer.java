718ec6c35ce53b3ec18b187ebc4843a3db20c00a
Mandeep <mandeepyadav0111@gmail.com>
2025-09-16 22:57:44 +0530

    Time: 2 ms (100%), Space: 45.1 MB (14.52%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int getvalue(char c){
+        switch(c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case  'X': return 10;
+            case 'L': return 50;
+            case   'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+    public int romanToInt(String s) {
+       int length=s.length();
+       int sum=0;
+        for(int i=0;i<length;i++){
+           int c=getvalue(s.charAt(i));
+            if((i+1)<length && c< getvalue(s.charAt(i+1)) ){
+               sum-=c;
+            }
+            else{
+                sum+=c;
+            }
+        }
+         return sum;
+        
+    }
+}
